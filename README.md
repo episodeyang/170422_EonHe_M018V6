eaaf493b8b2e3abc8940587cf8604e8f83656c55
Ge Yang <yangge1987@gmail.com>
2017-05-08 21:03:03 -0500

    add experiment class scafold

---
+++ README.md
@@ -0,0 +1,14 @@
+# Electrons on Helium M018V6 Experiment Repo
+
+To start experiments
+```bash
+pip install -r requirements.txt
+```
+
+Each experiment is configured via a `yaml` file.
+
+Author:
+- Ge Yang: yangge1987@gmail.com
+- Gerwin Koolstra
+
+University of Chicago
--- experiment/eonhe_experiment/eonhe_experiment.py
+++ experiment/eonhe_experiment/eonhe_experiment.py
@@ -2,8 +2,27 @@ __author__ = 'Ge Yang'
 import yaml
 
 class EonHeExperiment():
-    def __init__(self, prefix, config_file_path):
-        config =
+    def __init__(self, config_file_path=None):
+        with open(config_file_path, 'r') as config_file:
+            # save text of yaml config in datacache
+            # self.config_text = config_file.readlines()
+            self.config = yaml.load(config_file)
+
+    def configure(self, config=None):
+        if not config:
+            config = self.config
+        else:
+            # Note: not sure if overwriting is the right thing to do.
+            self.config = config
+
+    def make_data_dir(self, path):
+        if not os.path.isdir(path):
+            os.mkdir(path)
+
+    def init_filament(self, filament_parameters):
+        self.fil = filament.settup_driver()
 
 
 
+if __name__ == "__main__":
+    eonhe = EonHeExperiment('./eonhe-test.yml')
